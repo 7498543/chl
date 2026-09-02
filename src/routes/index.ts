7b72aa7efb8493bf88fbc7245b176753ce899d9d
q <3345150869@qq.com>
2026-09-02 15:44:38 +0800

    静态资源

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import path from "node:path";
 import baseRouter from "./router/base";
 
 const router = express.Router();
+
+router.use(express.static(path.resolve("public")));
 
 // TODO 自动导入所有路由
 
